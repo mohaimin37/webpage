f8a79e507da06614535a07500269d58d2a16ff56
mohaimin37 <137748062+mohaimin37@users.noreply.github.com>
2023-06-27 10:03:43 +0530

    Add files via upload

---
+++ webdevelopment/anas.js
@@ -0,0 +1,8 @@
+const anas = document.getElementById(one)
+const raju  = ('onclick',()=>{
+    alert("i am your father")
+})
+const raja = document.getElementById(two)
+const babu  = ('onclick',()=>{
+    alert("i am your father")
+})
